a4091a76263521c6f2d8d38696ad585835510779
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2023-10-10 10:49:58 +0200

    Conditionally use 'Suggests' packages in 'tests' files

--- tests/nbgraph.R
+++ tests/nbgraph.R
@@ -1,11 +1,11 @@
 library(ade4)
 library(adegraphics)
 library(sp)
-library(spdep)
 library(lattice)
 
 pdf("nbgraph.pdf")
 
+if(require(spdep)) {
 data(elec88, package = "ade4")
 coords <- sp::coordinates(elec88$Spatial)
 
@@ -14,3 +14,4 @@ xyplot(coords[, 2] ~ coords[, 1],
 
 g1 <- s.label(coords, nb = elec88$nb, porigin.include = F, plabels.cex = 0.7, 
               ppoints.cex = 2, Sp = elec88$Spatial, pSp.col = "red", pSp.alpha = 0.5)
+}
